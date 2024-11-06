ebf6a7e6015eb87fa86b1df4caf149348f55c433
Lysa14 <lysaderbah@gmail.com>
2024-11-06 11:28:39 +0100

    first test of cicd go

---
+++ function1.go
@@ -0,0 +1,6 @@
+package function1
+
+func Sum(x, y int) int {
+	z := x + y
+	return z
+}
---
+++ tests/test1.go
@@ -0,0 +1,14 @@
+package function1
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	want := 8
+	got := Sum(3, 5)
+
+	if got != want {
+		t.Errorf("Test fail! want: '%d', got: '%d'", want, got)
+	}
+
+}
