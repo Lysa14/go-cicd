088d1361f62808ee3c6febb5d3ba2689f2df7cfa
Lysa14 <lysaderbah@gmail.com>
2024-11-06 11:40:36 +0100

    refactor: update CI workflow and add greeting functionality with tests

--- function1.go
+++
@@ -1,6 +0,0 @@
-package function1
-
-func Sum(x, y int) int {
-	z := x + y
-	return z
-}
--- tests/test1.go
+++ tests/test1.go
@@ -1,14 +1,80 @@
-package function1
+package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestEmptyName(t *testing.T) {
+	name := ""
+	language := "english"
+	expected := "Hello World"
+	actual, err := Hello(name, language)
 
-	want := 8
-	got := Sum(3, 5)
-
-	if got != want {
-		t.Errorf("Test fail! want: '%d', got: '%d'", want, got)
+	if err != nil {
+		t.Errorf("Should not produce an error")
 	}
 
+	if expected != actual {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestEnglish(t *testing.T) {
+	name := "Ben"
+	language := "english"
+	expected := "Hello Ben"
+	actual, err := Hello(name, language)
+
+	if err != nil {
+		t.Errorf("Should not produce an error")
+	}
+
+	if expected != actual {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestSpanish(t *testing.T) {
+	name := "John"
+	language := "spanish"
+	expected := "Hola John"
+	actual, err := Hello(name, language)
+
+	if err != nil {
+		t.Errorf("Should not produce an error")
+	}
+
+	if expected != actual {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestGerman(t *testing.T) {
+	name := "Mary"
+	language := "german"
+	expected := "Hallo Mary"
+	actual, err := Hello(name, language)
+
+	if err != nil {
+		t.Errorf("Should not produce an error")
+	}
+
+	if expected != actual {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", actual, expected)
+	}
+}
+
+func TestUnsupportedLanguage(t *testing.T) {
+	name := "Maurice"
+	language := "french"
+	expected := "need to provide a supported language"
+	_, err := Hello(name, language)
+
+	if err == nil {
+		t.Errorf("Should produce an error for an unsupported language")
+	}
+
+	actual := err.Error()
+
+	if expected != actual {
+		t.Errorf("Error message was incorrect, got: %s, want: %s.", actual, expected)
+	}
 }
